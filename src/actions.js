937ee03529faef1c3598b46ee2674687c99fe9ad
Randall Reed, Jr <randallreedjr@gmail.com>
2016-12-07 17:09:33 -0500

    Fix typos

--- src/actions.js
+++ src/actions.js
@@ -10,7 +10,7 @@ export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
  * other constants
  */
 
-export const VisiblityFilters = {
+export const VisibilityFilters = {
   SHOW_ALL: 'SHOW_ALL',
   SHOW_COMPLETED: 'SHOW_COMPLETED',
   SHOW_ACTIVE: 'SHOW_ACTIVE'
--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,4 @@
-import { combineReduces } from 'redux';
+import { combineReducers } from 'redux';
 import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions';
 const { SHOW_ALL } = VisibilityFilters
 
