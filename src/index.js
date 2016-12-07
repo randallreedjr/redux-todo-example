435488a2cd0ac5f530b4f878321e129a0df76633
Randall Reed, Jr <randallreedjr@gmail.com>
2016-12-07 17:10:21 -0500

    Introduce createStore from Redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { createStore } from 'redux';
+import todoApp from './reducers';
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+let store = createStore(todoApp);
